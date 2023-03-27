e34be639069f42c54de5cc5661015c78258e098e
Chuf-lco <mainaantony07@gmail.com>
2023-03-27 14:09:09 -0700

    Formatted output

---
+++ format.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * _putchar - Printing a character
+ * @c: parameter
+ * Return: Return Character Success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _printf - Custom printf function
+ * @format: Parameters for output
+ * Return: Return Correct Output Success
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0;
+	char c, *s;
+
+	va_start(ap, format);
+	for (; *format != '\0'; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = (char) va_arg(ap, int);
+				_putchar(c);
+				i++;
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(ap, char *);
+				fputs(s, stdout);
+				i += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				i++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			i++;
+		}
+	}
+	va_end(ap);
+	return (i);
+}
