f989fe8d8c0440fd2d3986b9912152c4cc8941a7
Chuf-lco <mainaantony07@gmail.com>
2023-03-29 01:22:46 -0700

    Print  numbers

--- print_number.c
+++ print_number.c
@@ -5,24 +5,24 @@
  */
 void print_number(int32_t num)
 {
-        uint32_t u_num, divisor, digit;
+	uint32_t u_num, divisor, digit;
 
-        if (num < 0)
-        {
-                _putchar('-');
-                num = -num;
-        }
-        u_num = (uint32_t)num;
-        divisor = 1;
-        while (u_num / divisor > 9)
-        {
-                divisor *= 10;
-        }
-        while (divisor != 0)
-        {
-                digit = u_num / divisor;
-                _putchar('0' + digit);
-                u_num %= divisor;
-                divisor /= 10;
-        }
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	u_num = (uint32_t)num;
+	divisor = 1;
+	while (u_num / divisor > 9)
+	{
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		digit = u_num / divisor;
+		_putchar('0' + digit);
+		u_num %= divisor;
+		divisor /= 10;
+	}
 }
